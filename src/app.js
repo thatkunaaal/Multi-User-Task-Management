81f3b4f70aa3ec751aeab34e4b25164cf930b086
thatkunaaal <thatkunaal@gmail.com>
2025-08-24 17:01:27 +0530

    feat: get all task

--- src/app.js
+++ src/app.js
@@ -180,33 +180,27 @@ app.put("/tasks/:id", userAuth, async (req, res) => {
 
     if (!task) return res.status(404).json({ mesage: "Task not found" });
 
-    const title = req.body?.title;
-    const description = req.body?.description;
-    const status = req.body?.status;
+    const { title, description, status } = req.body;
 
-
-    if (!title) throw new Error("Please enter title ");
+    if (!title || !status) {
+      return res.status(400).json({ message: "Title and status are required" });
+    }
 
     // Checking status
     const allowedStatus = ["pending", "in_progress", "done"];
-    const checkedStatus = status ? status.trim() : "pending";
-
-    const isStatusValid = allowedStatus.includes(checkedStatus);
-
-    if (!isStatusValid) throw new Error("Status is not valid.");
+    const checkedStatus = status.trim().toLowerCase();
 
     const trimmedTitle = title.trim().toLowerCase();
     const trimmedDescription = description ? description.trim() : null;
 
-    const isTitleExist = await Task.findOne({
-        where:{
-            user_id : user.id,
-            title: trimmedTitle
-        }
-    })
+    // Checking existing task.
+    const existingTask = await Task.findOne({
+      where: { user_id: user.id, title: trimmedTitle },
+    });
 
-    if(trimmedTitle)
-        return res.status(401).json({message: "This title already exist"});
+    if (existingTask && existingTask.id !== task.id) {
+      return res.status(409).json({ message: "Title already exists for this user" });
+    }
 
     task.set({
       title: trimmedTitle,
@@ -216,11 +210,79 @@ app.put("/tasks/:id", userAuth, async (req, res) => {
 
     await task.save();
 
-    res.status(200).json({ message: "Task updated successfully" });
+    res.status(200).json({ message: "Task updated successfully" ,task});
   } catch (err) {
     res.status(400).json({ message: err.message });
   }
 });
+
+app.delete("/tasks/:id",userAuth,async (req,res) => {
+    try {
+        const user = req.user;
+        const taskId = parseInt(req.params.id);
+
+        // Validating task id.
+        if(isNaN(taskId))
+            return res.status(404).json({message: "Task not found."});
+
+        const task = await Task.findOne({
+            where: {
+                user_id : user.id,
+                id : taskId,
+            }
+        })
+
+        if(!task)
+            return res.status(404).json({message: "Task not found."});
+        
+        await task.destroy();
+
+        res.status(200).json({message: "Task has been successfully deleted.",
+            task
+        })
+        
+    } catch (err) {
+        res.status(400).json({message : err.mesage});
+    }
+})
+
+app.get("/tasks/",userAuth,async (req,res) => {
+    try {
+        
+        const user = req.user;
+        let page = parseInt(req.query.page) || 1;
+        let limit = parseInt(req.query.limit) || 10;
+
+        limit = (limit > 50) ? 50 : limit;
+        const skip = (page-1)*limit;
+
+        if(page<=0 || limit<=0){
+            page = 1;
+            limit = 10;
+        }
+
+        const {count , rows: tasks} = await Task.findAndCountAll({
+            where: {
+                user_id : user.id,
+            },
+            offset: skip,
+            limit,
+        })
+
+        if(tasks.length == 0)
+            return res.status(400).json({message : "Task not found."});
+
+        res.status(200).json({
+            taskCount : count,
+            tasks,
+            currentPage: page
+        })
+        
+
+    } catch (err) {
+        res.status(400).json({message : err.mesage});
+    }
+})
 
 // Connnecting DB before listening to the server. {BEST PRACTISE}
 connectDB()
